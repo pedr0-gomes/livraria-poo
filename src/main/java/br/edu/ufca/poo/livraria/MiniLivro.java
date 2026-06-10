e796de86830e7cc0c3d655907ae82d54c3301d61
Pedro Gomes Sampaio <gomes.pedro@aluno.ufca.edu.br>
2026-06-10 09:15:48 -0300

    Adiciona a classe MiniLivro estendendo Livro

---
+++ src/main/java/br/edu/ufca/poo/livraria/MiniLivro.java
@@ -0,0 +1,16 @@
+package br.edu.ufca.poo.livraria;
+
+public class MiniLivro extends Livro {
+    public MiniLivro(Autor autor) {
+        super(autor);
+    }
+
+    public MiniLivro(Autor autor, String nome, String descricao, double valor, int numeroPaginas, String isbn) {
+        super(autor, nome, descricao, valor, numeroPaginas, isbn);
+    }
+
+    @Override
+    public boolean aplicaDescontoDe(double porcentagem) {
+        return false;
+    }
+}
